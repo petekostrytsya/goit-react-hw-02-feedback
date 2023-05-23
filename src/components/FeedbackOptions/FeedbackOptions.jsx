fe1e73e214d19843f778ecd4a8970272618e62e8
petekostrytsya <petekostrytsya@gmail.com>
2023-05-23 23:47:05 +0300

    add Component Stats

---
+++ src/components/FeedbackOptions/FeedbackOptions.jsx
@@ -0,0 +1,8 @@
+import PropTypes from 'prop-types';
+import { } from './FeedbackOptions.styled';
+
+export const FeedbackOptions = ({ options, onLeaveFeedback }) => {
+    // return (
+        
+    // )
+}
---
+++ src/components/Statistics/Statistics.styled.jsx
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+
+export const StatsList = styled.ul`
+    margin: 0 auto;
+    width: 960px;
+    background-color: blue;
+    box-shadow: 0px 0px 10px 0px rgba(0, 0, 0, 0.5);
+`
+
+export const StatsItem = styled.li`
+    font-family: Oxygen;
+    font-size: 24px;
+    font-weight: Bold;
+    text-align: center;
+`
